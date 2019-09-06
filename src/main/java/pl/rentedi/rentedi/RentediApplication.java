e9e2a3103b9ddb530eaf394d043ee5daf1a0159b
Rafał Pilarczyk <rpilarczyk@gmail.com>
2019-09-06 14:14:37 +0200

    Init commit

---
+++ src/main/java/pl/rentedi/rentedi/RentediApplication.java
@@ -0,0 +1,13 @@
+package pl.rentedi.rentedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentediApplication.class, args);
+	}
+
+}
